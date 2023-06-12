485b880bbda0a24b1e4a9a5545a1cfd42390460c
Nerekk <arkadiusz.salaj.02@gmail.com>
2023-06-13 00:10:06 +0200

    Refactoring gameloop thread

--- src/main/java/com/example/checkers/GameLoop.java
+++ src/main/java/com/example/checkers/GameLoop.java
@@ -18,24 +18,33 @@ public class GameLoop implements Runnable{
     public Label countp2 = (Label) mainCont.lookup("#countp2");
     public Label timerp1 = (Label) mainCont.lookup("#timer1");
     public Label timerp2 = (Label) mainCont.lookup("#timer2");
+    public Label infop1 = (Label) mainCont.lookup("#infop1");
+    public Label infop2 = (Label) mainCont.lookup("#infop2");
+
     @Override
     public void run() {
-        while (true) {
-            if (isWhiteTurn) {
-                turn1.setVisible(true);
-                turn2.setVisible(false);
-            }
-            else {
-                turn1.setVisible(false);
-                turn2.setVisible(true);
-            }
-            Platform.runLater(() -> displayCount(capturesWhite, capturesBlack));
+        while (isGameON) {
+
+            Platform.runLater(() -> handleInfo(capturesWhite, capturesBlack));
             try {
                 Thread.sleep(200);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
+
+            if (checkWin()) {
+                Platform.runLater(this::displayWinner);
+                Platform.runLater(() -> handleInfo(capturesWhite, capturesBlack));
+                isGameON = false;
+            }
         }
+    }
+
+    public boolean checkWin() {
+        if (capturesWhite == 12) whiteWon = true;
+        if (capturesBlack == 12) blackWon = true;
+
+        return whiteWon || blackWon;
     }
 
     public AnchorPane getMainContainer() {
@@ -44,7 +53,23 @@ public class GameLoop implements Runnable{
         return (AnchorPane) container;
     }
 
-    public void displayCount(int p1, int p2) {
+    public void handleInfo(int p1, int p2) {
+        String waitTurn = "Waiting for turn..";
+        String moveTurn = "Your turn!";
+
+        if (isWhiteTurn && isGameON) {
+            turn1.setVisible(true);
+            infop1.setText(moveTurn);
+            turn2.setVisible(false);
+            infop2.setText(waitTurn);
+        }
+        else if (!isWhiteTurn && isGameON) {
+            turn1.setVisible(false);
+            infop1.setText(waitTurn);
+            turn2.setVisible(true);
+            infop2.setText(moveTurn);
+        }
+
         countp1.setText(String.valueOf(p1));
         countp2.setText(String.valueOf(p2));
 
@@ -52,5 +77,19 @@ public class GameLoop implements Runnable{
         timerp1.setText(time);
         time = String.format("%02d:%02d", blackSecondsLeft / 60, blackSecondsLeft % 60);
         timerp2.setText(time);
+    }
+
+    public void displayWinner() {
+        String win = "You won!";
+        String lose = "You lost!";
+
+        if (whiteWon) {
+            infop1.setText(win);
+            infop2.setText(lose);
+        }
+        if (blackWon) {
+            infop1.setText(lose);
+            infop2.setText(win);
+        }
     }
 }
