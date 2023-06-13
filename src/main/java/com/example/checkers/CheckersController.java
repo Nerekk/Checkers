754552cda023c1351b5bc5554a5219a8249a2f26
Nerekk <arkadiusz.salaj.02@gmail.com>
2023-06-14 01:00:35 +0200

    Added options scene

--- src/main/java/com/example/checkers/CheckersController.java
+++ src/main/java/com/example/checkers/CheckersController.java
@@ -12,26 +12,40 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Screen;
 
 import static com.example.checkers.Checkers.getPrimaryStage;
+import static com.example.checkers.Game.enemyType;
+import static com.example.checkers.Game.isServer;
 
 public class CheckersController {
+    public static String GAMESCENE = "game-scene.fxml";
+    public static String OPTIONSCENE = "options-scene.fxml";
     public static void setScene(String sceneFXML) {
         Screen screen = Screen.getPrimary();
         Rectangle2D bounds = screen.getVisualBounds();
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(Checkers.class.getResource(sceneFXML));
-            getPrimaryStage().setScene(new Scene(fxmlLoader.load(), bounds.getWidth(), bounds.getHeight() - 20));
+            getPrimaryStage().setScene(new Scene(fxmlLoader.load()));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
 
-//    @FXML
-//    private void mouseEntered(MouseEvent e) {
-//        Node source = (Node)e.getSource() ;
-//        Integer colIndex = GridPane.getColumnIndex(source);
-//        Integer rowIndex = GridPane.getRowIndex(source);
-//        System.out.printf("Mouse entered cell [%d, %d]%n", colIndex.intValue(), rowIndex.intValue());
-//    }
+    @FXML
+    public void prepareGameServer() {
+        setScene(GAMESCENE);
+        Game game = new Game(true, 1);
+    }
+
+    @FXML
+    public void prepareGameClient() {
+        setScene(GAMESCENE);
+        Game game = new Game(false, 1);
+    }
+
+    @FXML
+    public void prepareGameLocal() {
+        setScene(GAMESCENE);
+        Game game = new Game(false, 0);
+    }
 
 
 }
