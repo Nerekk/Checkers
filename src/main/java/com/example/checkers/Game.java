623907728d934ec0f1773ce6252e0eaebdf3affc
Nerekk <arkadiusz.salaj.02@gmail.com>
2023-06-13 20:05:55 +0200

    Code refactoring3

--- src/main/java/com/example/checkers/Game.java
+++ src/main/java/com/example/checkers/Game.java
@@ -886,9 +886,7 @@ public class Game {
                 for (int row = 0; row < SIZE; row++) {
                     for (int col = 0; col < SIZE; col++) {
                         Circle circle;
-                        if ((col + 2) % 2 == 0 && row % 2 == 0 || (col + 1) % 2 == 0 && row % 2 == 1) {
-                            circle = createCircle(true);
-                        } else {
+                        if (!((col + 2) % 2 == 0 && row % 2 == 0 || (col + 1) % 2 == 0 && row % 2 == 1)) {
                             if (row == 0 || row == 1 || row == 2)
                             {
                                 circle = createCircle(false);
